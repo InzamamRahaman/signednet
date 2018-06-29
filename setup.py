f30a201ef3344fcd1c02a84f15a8176f24c4ea53
InzamamRahaman <izzyrahaman@gmail.com>
2018-06-29 14:06:43 -0400

    structure for setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+long_description = """
+    A collection of utilities built on networkx for signed networks
+"""
+
+
+
+setup(
+    name='signednet',
+    version='0.1dev',
+    packages=['signednet'],
+    license='MIT',
+    long_description=long_description
+)
